d98d01d793fd1952ed390ce7f2ee25a49fcf1697
teste <teste@10.32.175.176>
2018-05-03 14:31:31 -0300

    First one

---
+++ artefato02/src/main/java/s2bteste/artefato02/Hello.java
@@ -0,0 +1,22 @@
+package s2bteste.artefato02;
+public class Hello {
+	public static void main(String[]args) {			
+		int a = 20;
+		int b = 30;				
+		
+		if(somar(a,b)==50) {
+			System.out.println("Sucesso!");						
+		}else {
+			System.out.println("Falha!");
+		}
+	}
+	
+	public static int somar(int a, int b) {		
+		return a+b;		
+	}
+	
+	public static int subtracao(int a, int b) {		
+		return a-b;		
+	}
+	
+}
