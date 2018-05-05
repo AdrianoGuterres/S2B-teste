af907f0655d7a54a34b5db0fb372dac6371c019e
Adriano <Adriano@192.168.15.5>
2018-05-05 14:40:07 -0300

    Removendo a biblioteca JUnit 5

--- artefato02/src/test/java/sbteste/artefato02/testes/HelloTest.java
+++ artefato02/src/test/java/sbteste/artefato02/testes/HelloTest.java
@@ -1,8 +1,8 @@
 package sbteste.artefato02.testes;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import s2bteste.artefato02.code.Hello;
 
